d7afb9d504572e6dc62939ffb206ef2e4c842d9b
sierr <sierrayavari@email.arizona.edu>
2020-02-13 17:26:33 -0700

    Fixed module error

---
+++ Homepage.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+window = Tk()
+window.geometry('800x400')
+
+window.title("Homepage")
+
+top_frame = Frame(window).pack()
+bottom_frame = Frame(window).pack(side = "bottom")
+
+label = Label(window, text = "Please select an option below:").pack()
+
+button_test = Button(top_frame, text = "Take Test", fg = "black").pack(side = "top")
+
+button_admin = Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black").pack()
+
+button_deskTop = Button(bottom_frame, text = "Exit to Desktop", fg = "black").pack(side = "right")
+
+window.mainloop()
