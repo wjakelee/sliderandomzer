8bf836973bd3d03d58292e1a030c58c287271f11
sierrayavari <60908298+sierrayavari@users.noreply.github.com>
2020-02-13 17:21:35 -0700

    uploading homepage python file

---
+++ Homepage.py
@@ -0,0 +1,19 @@
+import Tkinter
+
+window = Tkinter.Tk()
+window.geometry('800x400')
+
+window.title("Homepage")
+
+top_frame = Tkinter.Frame(window).pack()
+bottom_frame = Tkinter.Frame(window).pack(side = "bottom")
+
+label = Tkinter.Label(window, text = "Please select an option below:").pack()
+
+button_test = Tkinter.Button(top_frame, text = "Take Test", fg = "black").pack(side = "top-right")
+
+button_admin = Tkinter.Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black").pack()
+
+button_deskTop = Tkinter.Button(bottom_frame, text = "Exit to Desktop", fg = "black").pack(side = "right")
+
+window.mainloop()
