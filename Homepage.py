74050d181c53d252e4e36cf08fa81f05aece56f4
sierr <sierrayavari@email.arizona.edu>
2020-02-17 11:31:12 -0700

    Fixed button location

--- Homepage.py
+++ Homepage.py
@@ -11,10 +11,9 @@ bottom_frame = Frame(window).pack(side = "bottom")
 
 label = Label(window, text = "Please select an option below:").pack()
 
-button_test = Button(top_frame, text = "Take Test", fg = "black").pack(side = "top")
+button_test = Button(top_frame, text = "Take Test", fg = "black").place(relx=0.5, rely=0.5, anchor=CENTER)
 
-button_admin = Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black").pack()
+button_admin = Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black").place(relx=0.7, rely=0.5, anchor=CENTER)
 
-button_deskTop = Button(bottom_frame, text = "Exit to Desktop", fg = "black").pack(side = "right")
-
+button_deskTop = Button(bottom_frame, text = "Exit to Desktop", fg = "black").place(relx=1.0, rely=1.0, anchor=SE)
 window.mainloop()
