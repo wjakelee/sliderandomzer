6fb705a4e5f33b27c4a2d8d107609db54133380b
Jake Lee <jwlee@email.arizona.edu>
2020-02-20 22:17:30 -0700

    First five case entries added. Entry data saves to CSV.

--- test_setup.py
+++ test_setup.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import csv
 
+
 set_wind = Tk()
 set_wind.title("Test Setup")                    # window title
 set_wind.geometry('800x480')                    # window geometry
@@ -21,6 +22,13 @@ scroll.pack(side='right', fill='y')                     # scroll bar placement
 
 Label(scrollable_frame, text="TEST SETUP", background='white', font='Arial 20 bold underline').grid(row=1, column=1)
 
+
+fields = ['Case', 'Reagent Type', 'h&e Slide', 'Nrc Slide', 'Ab Slide']
+with open('case_information.csv', 'w') as caseinfo_csv:
+    csv_writer = csv.writer(caseinfo_csv)
+    csv_writer.writerow(fields)
+
+
 # ----- Begin case 1 Entry
 
 Label(scrollable_frame, text="\nCASE 1", background='white', font='Arial 12 underline').grid(row=2, column=1)
@@ -40,19 +48,23 @@ Entry(scrollable_frame, textvariable=c1ab, background='#e6e6e6').grid(row=5, col
 
 
 def c1entry():      # function saves entry data for case 1 to CSV file
+    Label(scrollable_frame, text="SAVED", background='white', font='Arial 12').grid(row=4, column=5)
     a = c1rt.get()
     b = c1he.get()
     c = c1nrc.get()
     d = c1ab.get()
-    print(a)
-    print(b)
-    print(c)
-    print(d)
+
+    list = ['1', a, b, c, d]
+    with open('case_information.csv', 'a') as caseinfo_csv:
+        csv_writer = csv.writer(caseinfo_csv)
+        csv_writer.writerow(list)
 
 
 Button(scrollable_frame, text="Save", background='green', font='Arial 12 bold', command=c1entry).grid(row=4, column=1)      # call save entry data function for C1
 
 # ----- End case 1 Entry
+
+
 
 # ----- Begin case 2 Entry
 
@@ -73,19 +85,133 @@ Entry(scrollable_frame, textvariable=c2ab, background='#e6e6e6').grid(row=9, col
 
 
 def c2entry():      # function saves entry data for case 1 to CSV file
-    a = c1rt.get()
-    b = c1he.get()
-    c = c1nrc.get()
-    d = c1ab.get()
-    print(a)
-    print(b)
-    print(c)
-    print(d)
+    Label(scrollable_frame, text="SAVED", background='white', font='Arial 12').grid(row=8, column=5)
+    a = c2rt.get()
+    b = c2he.get()
+    c = c2nrc.get()
+    d = c2ab.get()
+
+    list = ['2', a, b, c, d]
+    with open('case_information.csv', 'a') as caseinfo_csv:
+        csv_writer = csv.writer(caseinfo_csv)
+        csv_writer.writerow(list)
 
 
 Button(scrollable_frame, text="Save", background='green', font='Arial 12 bold', command=c2entry).grid(row=8, column=1)      # call save entry data function for C2
 
-# ---- End case 3 entry
+# ----- End case 2 entry
+
+
+
+# ----- Begin case 3 Entry
+
+Label(scrollable_frame, text="\nCASE 3", background='white', font='Arial 12 underline').grid(row=10, column=1)
+Label(scrollable_frame, text="Reagent Type:", background='white', font='Arial 12').grid(row=11, column=2)
+Label(scrollable_frame, text="       h&e:", background='white', font='Arial 12').grid(row=11, column=3)
+Label(scrollable_frame, text="       Nrc:", background='white', font='Arial 12').grid(row=12, column=3)
+Label(scrollable_frame, text="       Ab:", background='white', font='Arial 12').grid(row=13, column=3)
+
+c3rt = StringVar()      # change variable type to string for user entry
+c3he = StringVar()
+c3nrc = StringVar()
+c3ab = StringVar()
+Entry(scrollable_frame, textvariable=c3rt, background='#e6e6e6').grid(row=12, column=2)      # user entry bars
+Entry(scrollable_frame, textvariable=c3he, background='#e6e6e6').grid(row=11, column=4)
+Entry(scrollable_frame, textvariable=c3nrc, background='#e6e6e6').grid(row=12, column=4)
+Entry(scrollable_frame, textvariable=c3ab, background='#e6e6e6').grid(row=13, column=4)
+
+
+def c3entry():      # function saves entry data for case 1 to CSV file
+    Label(scrollable_frame, text="SAVED", background='white', font='Arial 12').grid(row=12, column=5)
+    a = c3rt.get()
+    b = c3he.get()
+    c = c3nrc.get()
+    d = c3ab.get()
+
+    list = ['3', a, b, c, d]
+    with open('case_information.csv', 'a') as caseinfo_csv:
+        csv_writer = csv.writer(caseinfo_csv)
+        csv_writer.writerow(list)
+
+
+Button(scrollable_frame, text="Save", background='green', font='Arial 12 bold', command=c3entry).grid(row=12, column=1)      # call save entry data function for C2
+
+# ----- End case 3 entry
+
+
+
+# ----- Begin case 4 Entry
+
+Label(scrollable_frame, text="\nCASE 4", background='white', font='Arial 12 underline').grid(row=14, column=1)
+Label(scrollable_frame, text="Reagent Type:", background='white', font='Arial 12').grid(row=15, column=2)
+Label(scrollable_frame, text="       h&e:", background='white', font='Arial 12').grid(row=15, column=3)
+Label(scrollable_frame, text="       Nrc:", background='white', font='Arial 12').grid(row=16, column=3)
+Label(scrollable_frame, text="       Ab:", background='white', font='Arial 12').grid(row=17, column=3)
+
+c4rt = StringVar()      # change variable type to string for user entry
+c4he = StringVar()
+c4nrc = StringVar()
+c4ab = StringVar()
+Entry(scrollable_frame, textvariable=c4rt, background='#e6e6e6').grid(row=16, column=2)      # user entry bars
+Entry(scrollable_frame, textvariable=c4he, background='#e6e6e6').grid(row=15, column=4)
+Entry(scrollable_frame, textvariable=c4nrc, background='#e6e6e6').grid(row=16, column=4)
+Entry(scrollable_frame, textvariable=c4ab, background='#e6e6e6').grid(row=17, column=4)
+
+
+def c4entry():      # function saves entry data for case 1 to CSV file
+    Label(scrollable_frame, text="SAVED", background='white', font='Arial 12').grid(row=16, column=5)
+    a = c4rt.get()
+    b = c4he.get()
+    c = c4nrc.get()
+    d = c4ab.get()
+
+    list = ['4', a, b, c, d]
+    with open('case_information.csv', 'a') as caseinfo_csv:
+        csv_writer = csv.writer(caseinfo_csv)
+        csv_writer.writerow(list)
+
+
+Button(scrollable_frame, text="Save", background='green', font='Arial 12 bold', command=c4entry).grid(row=16, column=1)      # call save entry data function for C2
+
+# ----- End case 4 entry
+
+
+
+# ----- Begin case 5 Entry
+
+Label(scrollable_frame, text="\nCASE 5", background='white', font='Arial 12 underline').grid(row=18, column=1)
+Label(scrollable_frame, text="Reagent Type:", background='white', font='Arial 12').grid(row=19, column=2)
+Label(scrollable_frame, text="       h&e:", background='white', font='Arial 12').grid(row=19, column=3)
+Label(scrollable_frame, text="       Nrc:", background='white', font='Arial 12').grid(row=20, column=3)
+Label(scrollable_frame, text="       Ab:", background='white', font='Arial 12').grid(row=21, column=3)
+
+c5rt = StringVar()      # change variable type to string for user entry
+c5he = StringVar()
+c5nrc = StringVar()
+c5ab = StringVar()
+Entry(scrollable_frame, textvariable=c5rt, background='#e6e6e6').grid(row=20, column=2)      # user entry bars
+Entry(scrollable_frame, textvariable=c5he, background='#e6e6e6').grid(row=19, column=4)
+Entry(scrollable_frame, textvariable=c5nrc, background='#e6e6e6').grid(row=20, column=4)
+Entry(scrollable_frame, textvariable=c5ab, background='#e6e6e6').grid(row=21, column=4)
+
+
+def c5entry():      # function saves entry data for case 1 to CSV file
+    Label(scrollable_frame, text="SAVED", background='white', font='Arial 12').grid(row=20, column=5)
+    a = c5rt.get()
+    b = c5he.get()
+    c = c5nrc.get()
+    d = c5ab.get()
+
+    list = ['5', a, b, c, d]
+    with open('case_information.csv', 'a') as caseinfo_csv:
+        csv_writer = csv.writer(caseinfo_csv)
+        csv_writer.writerow(list)
+
+
+Button(scrollable_frame, text="Save", background='green', font='Arial 12 bold', command=c5entry).grid(row=20, column=1)      # call save entry data function for C2
+
+# ----- End case 5 entry
+
 
 
 set_wind.mainloop()
