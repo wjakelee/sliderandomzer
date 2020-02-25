5fab5946f046127e1a4beff6dd5270df683a96f5
sierr <sierrayavari@email.arizona.edu>
2020-02-24 21:29:51 -0700

    Changing button colors

--- Homepage.py
+++ Homepage.py
@@ -11,9 +11,9 @@ bottom_frame = Frame(window).pack(side = "bottom")
 
 label = Label(window, text = "Please select an option below:").pack()
 
-button_test = Button(top_frame, text = "Take Test", fg = "black").place(relx=0.5, rely=0.5, anchor=CENTER)
+button_test = Button(top_frame, text = "Take Test", fg = "black", bg = "green").place(relx=0.4, rely=0.5, anchor=CENTER)
 
-button_admin = Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black").place(relx=0.7, rely=0.5, anchor=CENTER)
+button_admin = Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black", bg = "blue").place(relx=0.6, rely=0.5, anchor=CENTER)
 
-button_deskTop = Button(bottom_frame, text = "Exit to Desktop", fg = "black").place(relx=1.0, rely=1.0, anchor=SE)
+button_deskTop = Button(bottom_frame, text = "Exit to Desktop", fg = "black", bg = "red").place(relx=1.0, rely=1.0, anchor=SE)
 window.mainloop()
