066114b00f5e3817ddf3ddc33fb9d8fc1c1f9ccc
sierr <sierrayavari@email.arizona.edu>
2020-02-25 22:23:31 -0700

    Changing button colors and locations to better match CDR pics

--- Homepage.py
+++ Homepage.py
@@ -6,14 +6,16 @@ window.geometry('800x400')
 
 window.title("Homepage")
 
-top_frame = Frame(window).pack()
-bottom_frame = Frame(window).pack(side = "bottom")
+top_frame = Frame(window, background='white')
+top_frame.place(anchor='nw', relwidth=1, relheight=1)
 
-label = Label(window, text = "Please select an option below:").pack()
 
-button_test = Button(top_frame, text = "Take Test", fg = "black", bg = "green").place(relx=0.4, rely=0.5, anchor=CENTER)
+Label(top_frame, text = "Please select an option below:",font = 'Arial 20 bold', bg = "white").place(relx=0.5, rely=0.1, anchor = 'n')
 
-button_admin = Button(top_frame, text = "Setup, DataFile, Randomization (Admin Only)", fg = "black", bg = "blue").place(relx=0.6, rely=0.5, anchor=CENTER)
+Button(top_frame, text = "Take Test", fg = "black",font = 'Arial 14 bold', width = '9', height = '3', bg = "#33cc33").place(relx=0.355, rely=0.5, anchor=CENTER)
 
-button_deskTop = Button(bottom_frame, text = "Exit to Desktop", fg = "black", bg = "red").place(relx=1.0, rely=1.0, anchor=SE)
+Button(top_frame, text = "Setup, DataFile,\nRandomization (Admin Only)", font = 'Arial 14 bold', width = '22', height = '3', fg = "black", bg = "#6699ff").place(relx=0.6, rely=0.5, anchor=CENTER)
+
+Button(top_frame, text = "Exit to Desktop", fg = "black", bg = "#ff4d4d").place(relx=1.0, rely=1.0, anchor=SE)
+
 window.mainloop()
