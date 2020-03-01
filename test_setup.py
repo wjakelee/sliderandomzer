fbcf9bc834e6235d8575f18571d34af6d86ac740
Jake Lee <jwlee@email.arizona.edu>
2020-02-29 17:53:42 -0700

    Selected CSV file saves file information to dictionary.
    Once a file is selected, the information in the file is saved to a dictionary. They user is then able to scan a slide and read the slides barcode. I still need to make it so the dictionary values are then saved in a class. Also need to make it so when a barcode is scanned it compares the scanned barcode to the barcodes saved in the class.

--- test_setup.py
+++ test_setup.py
@@ -14,21 +14,52 @@ frame.place(anchor='nw', relwidth=1, relheight=1)       # Frame placement
 Label(frame, text="TEST SETUP", background='white', font='Arial 20 bold underline').place(anchor='n', relx=.5)
 
 
-def openfile():         # opens file selection window and saves selected file to dictionary
+class Data:
+    def __init__(self, case, he, nrc, ab):
+        self.case = case
+        self.he = he
+        self.nrc = nrc
+        self.ab = ab
+
+
+# function will compare scanned barcode to the barcodes saved from setup file
+def compare():
+    Label(frame, text="Place scanned slide in the illuminated slot. Slot #.", background='white',
+          font='Arial 12 bold').place(anchor='n', relx=.5, rely=.65)
+
+
+# function opens file selection and saves selected csv file information to dictionary
+def selectfile():
     selected_file = filedialog.askopenfilename(title="Select a CSV File",       # opens file selector window
-                                      filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
+                    filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
 
+    dict = {}                                                       # initialize empty dictionary
     with open(selected_file, 'r', newline='') as setupfile:         # opens selected file
-        setupfile_reader = csv.DictReader(setupfile)                # reads selected file to dictionary
+        setupfile_reader = csv.reader(setupfile)                    # reads selected file to dictionary
+        next(setupfile_reader)                                      # goes to second line of csv file
         for row in setupfile_reader:
-            print(row)
+            dict[row[0]] = {'he': row[1], 'nrc': row[2], 'ab': row[3]}          # saves csv information to dictionary
+
+    print(dict)
+
             # Need to save each value from the CSV file to instance variables of a class
-    print(list)
+
+    # asks user to scan and read barcode once setup file is selected
+    Label(frame, text="Setup file read.\nScan and read a slide to determine its corresponding case and slot:",
+          background='white', font='Arial 12 bold').place(anchor='n', relx=.5, rely=.325)
+
+    barcode = StringVar()
+    Entry(frame, textvariable=barcode, background='#e6e6e6', font='Arial 20', ).place(anchor='n', relx=.575,
+            rely=.45, width='450', height='70')
+
+    # button to read the scanned barcode, command calls 'compare' function
+    Button(frame, text='Read\nBarcode', font='Arial 12 bold', bg='#00cc66', activebackground='#80ffbf',
+           height='3', width='10', command=compare).place(anchor='n', relx=.2, rely=.45)
 
 
-Button(frame, text='Select Setup File to Read', font='Arial 12', bg='#80bfff', activebackground='#ccebff',
-       height='3', width='30', command=openfile).place(anchor='n', relx=.5, rely=.15)
-
+# button to select setup file, command calls 'selectfile' function
+Button(frame, text='Select Setup File', font='Arial 12 bold', bg='#80bfff', activebackground='#ccebff',
+       height='3', width='30', command=selectfile).place(anchor='n', relx=.5, rely=.15)
 
 
 
