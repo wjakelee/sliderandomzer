a5f78475c60721866c44eb70303b7f2f43735b94
Jake Lee <jwlee@email.arizona.edu>
2020-03-01 19:00:21 -0700

    Import randomization and Generate randomization buttons now work.
    When either button is pressed, a randomization order is created/assigned.

--- Randomization_intro.py
+++ Randomization_intro.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import csv
+import random
 
 window = Tk()
 window.geometry('800x400')
@@ -9,11 +11,36 @@ top_frame = Frame(window, background='white')
 top_frame.place(anchor='nw', relwidth=1, relheight=1)
 
 
-Button(top_frame, text = "Import Randomization Order", fg = "black",font = 'Arial 14 bold', width = '25', height = '3', bg = "#cc99ff").place(relx=0.5, rely=0.25, anchor=CENTER)
+# generates a list of random numbers from 1-60 w/o repeats
+def generate_random():
+    with open('case_information.csv', 'r') as setupfile:
+        reader = csv.reader(setupfile)
+        next(reader)
+        import_list = []
+        for row in reader:
+            import_list.append(row[0])
+    random.shuffle(import_list)
+    print(import_list)
 
-Button(top_frame, text = "Generate Randomization Order", font = 'Arial 14 bold', width = '25', height = '3', fg = "black", bg = "#ffcc66").place(relx=0.5, rely=0.5, anchor=CENTER)
 
-Button(top_frame, text = "Export Data", font = 'Arial 14 bold', width = '25', height = '3', fg = "black", bg = "#99ff66").place(relx=0.5, rely=0.75, anchor=CENTER)
+def import_random():
+    with open('case_information.csv', 'r') as setupfile:
+        reader = csv.reader(setupfile)
+        next(reader)
+        import_list = []
+        for row in reader:
+            import_list.append(row[0])
+    print(import_list)
+
+
+Button(top_frame, text = "Import Randomization Order", fg = "black",font = 'Arial 14 bold', width = '25',
+       height = '3', bg = "#cc99ff", command=import_random).place(relx=0.5, rely=0.25, anchor=CENTER)
+
+Button(top_frame, text = "Generate Randomization Order", font = 'Arial 14 bold', width = '25', height = '3',
+       fg = "black", bg = "#ffcc66", command=generate_random).place(relx=0.5, rely=0.5, anchor=CENTER)
+
+Button(top_frame, text = "Export Data", font = 'Arial 14 bold', width = '25', height = '3',
+       fg = "black", bg = "#99ff66").place(relx=0.5, rely=0.75, anchor=CENTER)
 
 Button(top_frame, text = "Exit to Desktop", fg = "black", bg = "#ff4d4d").place(relx=1.0, rely=1.0, anchor=SE)
 
