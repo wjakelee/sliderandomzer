ce31feec7914125b8dacdee106c3c3430f513ec8
sierr <sierrayavari@email.arizona.edu>
2020-03-05 10:07:41 -0700

    created setup page for test and data files and randomization

--- Test_setup_and_Data_file_and_Randomization_homepage.py
+++ Test_setup_and_Data_file_and_Randomization_homepage.py
@@ -13,7 +13,6 @@ Button(top_frame, text = "Test Setup", fg = "black",font = 'Arial 14 bold', widt
 
 Button(top_frame, text = "Data File & Randomization", font = 'Arial 14 bold', width = '25', height = '3', fg = "black", bg = "#2E9AFE").place(relx=0.5, rely=0.5, anchor=CENTER)
 
-
 Button(top_frame, text = "Back To Home", fg = "black", bg = "#81DAF5").place(relx=1.0, rely=1.0, anchor=SE)
 
 window.mainloop()
