fccc7195dd523aea0ed8c2f82ece86899b83857c
jakelee <jwlee@email.arizona.edu>
2020-03-10 14:18:11 -0700

    Fixed: The username and password are not portrayed when trying to log back in.

--- Login.py
+++ Login.py
@@ -19,7 +19,7 @@ class Login(Frame):  # Sample Login page
 
         # button commands check_entry function
         Button(self, text="Enter", font='Arial 16 bold', fg="black", bg="grey", height='3', width='10',
-               command=lambda: check_entry(username.get(), password.get())).place(relx=0.5, rely=0.7, anchor='n')
+               command=lambda: check_entry(username, password)).place(relx=0.5, rely=0.7, anchor='n')
 
         # button calls show_frame method and takes you page to Start Page
         Button(self, text="Back To Home", fg="black", bg="#81DAF5", font="Arial 14", width='15', height='2',
@@ -28,8 +28,9 @@ class Login(Frame):  # Sample Login page
         # Function checks to see if the entered username/password are correct and calls show_frame module to raise
         # the SetDatRand frame
         def check_entry(username, password):
-            if username == "admin" and password == "password":
-                # reset username and password variable ""
+            if username.get() == "admin" and password.get() == "password":
+                username.set("")
+                password.set("")
                 controller.show_frame("SetDatRand")                   # raises SetDatRand frame
 
 # need to figure how to run this module on its own
