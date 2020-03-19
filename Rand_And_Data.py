641015c43ade94f63bfe6246e22bc77bef464962
Jake Lee <jwlee@email.arizona.edu>
2020-03-18 19:25:36 -0700

    A message box pops up when a randomization order is selected.
    If the user has not set up the test yet, then they cannot select a randomization order.

--- Rand_And_Data.py
+++ Rand_And_Data.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import csv
 import random
 
@@ -16,7 +17,15 @@ class RandAndFile(Frame):
                 for row in reader:
                     import_list.append(row[0])
             random.shuffle(import_list)
-            print(import_list)
+
+            if import_list == []:
+                messagebox.showerror(title="No Order Selected!", message="Please set up the test prior to "
+                                                                         "selecting a randomization order.")
+            else:
+                messagebox.showinfo(title="Order Selected", message="Randomization order has been selected and "
+                                                                    "re-randomized from the test setup file "
+                                                                    "(case_information.csv).")
+            # print(import_list)
 
         # imports order of numbers from temporary csv file
         def import_random():
@@ -25,23 +34,30 @@ class RandAndFile(Frame):
                 import_list = []
                 for row in reader:
                     import_list.append(row[0])
-            print(import_list)
+
+            if import_list == []:
+                messagebox.showerror(title="No Order Selected!", message="Please set up the test prior to "
+                                                                         "selecting a randomization order.")
+            else:
+                messagebox.showinfo(title="Order Selected", message="Randomization order has been selected from"
+                                                                    " the test setup file (case_information.csv).")
+            # print(import_list)
 
         Button(self, text="Import Randomization Order", fg="black", font='Arial 14 bold', width='30',
-               height='3', bg="#cc99ff", command=import_random).place(relx=0.5, rely=0.25, anchor=CENTER)
+               height='3', bg="#cc99ff", command=import_random).place(relx=0.5, rely=0.2, anchor=CENTER)
 
         Button(self, text="Generate Randomization Order", font='Arial 14 bold', width='30', height='3',
-               fg="black", bg="#ffcc66", command=generate_random).place(relx=0.5, rely=0.5, anchor=CENTER)
+               fg="black", bg="#ffcc66", command=generate_random).place(relx=0.5, rely=0.45, anchor=CENTER)
 
         Button(self, text="Export Data", font='Arial 14 bold', width='30', height='3',
-               fg="black", bg="#99ff66").place(relx=0.5, rely=0.75, anchor=CENTER)
+               fg="black", bg="#99ff66").place(relx=0.5, rely=0.7, anchor=CENTER)
 
         # button calls show_frame method and takes you page to Start Page
-        Button(self, text="Back To Home", fg="black", bg="#81DAF5", font="Arial 14", width='15', height='2',
+        Button(self, text="Back To Home", fg="black", bg="#81DAF5", font="Arial 14", width='12', height='1',
                command=lambda: controller.show_frame("StartPage")).place(relx=1.0, rely=1.0, anchor=SE)
 
         # button calls show_frame method and takes you back one page to SetDatRand
-        Button(self, text="Back", fg="black", bg="#81DAF5", font="Arial 14", width='15', height='2',
+        Button(self, text="Back", fg="black", bg="#81DAF5", font="Arial 14", width='12', height='1',
                command=lambda: controller.show_frame("SetDatRand")).place(relx=0.8, rely=1.0, anchor=SE)
 
 
