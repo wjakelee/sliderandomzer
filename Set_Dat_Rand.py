750eb4d3bf742fb77b4836dd232ad395751f6b41
Jake Lee <jwlee@email.arizona.edu>
2020-03-18 19:32:34 -0700

    Added a prompt for the user.

--- Set_Dat_Rand.py
+++ Set_Dat_Rand.py
@@ -6,13 +6,16 @@ class SetDatRand(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
 
+        Label(self, text="Select an option below:", font='Arial 20 bold').place(relx=0.5, rely=0.1,
+                                                                                       anchor='n')
+
         # button calls show_frame method and takes you to TestSetup page
         Button(self, text="Test Setup", fg="black", font='Arial 14 bold', width='25', height='3',
-               bg="#47d147", command=lambda: controller.show_frame("TestSetup")).place(relx=0.5, rely=0.25, anchor=CENTER)
+               bg="#47d147", command=lambda: controller.show_frame("TestSetup")).place(relx=0.5, rely=0.35, anchor=CENTER)
 
         # button takes you to Data File and Randomization page
         Button(self, text="Data File & Randomization", font='Arial 14 bold', width='25', height='3', fg="black",
-               bg="#80bfff", command=lambda: controller.show_frame("RandAndFile")).place(relx=0.5, rely=0.5, anchor=CENTER)
+               bg="#80bfff", command=lambda: controller.show_frame("RandAndFile")).place(relx=0.5, rely=0.6, anchor=CENTER)
 
         # button calls show_frame method and takes you to Start Page
         Button(self, text="Back To Home", fg="black", bg="#81DAF5", font="Arial 14", width='12', height='1',
