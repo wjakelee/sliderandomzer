0b9d598db79d86a970fed642e9887a29857ee5d2
Jake Lee <jwlee@email.arizona.edu>
2020-03-26 12:55:31 -0700

    Changed button colors.

--- Confirmation_Page.py
+++ Confirmation_Page.py
@@ -10,9 +10,9 @@ class ConfirmationPage(Frame):
                                                                                                 rely=0.1, anchor='n')
 
         Button(self, text="Yes.\nI am finished.", fg="black", font="Arial 14 bold", width="25", height="3",
-               bg="#01DF3A", command=lambda: controller.show_frame("TestCompletePage")).place(relx=.5, rely=.4,
+               bg="#47d147", command=lambda: controller.show_frame("TestCompletePage")).place(relx=.5, rely=.4,
                                                                                               anchor=CENTER)
 
         Button(self, text="No.\nReturn to test.", fg="black", font="Arial 14 bold", width="25", height="3",
-               bg="#FF0000", command=lambda: controller.show_frame("QuestionPage")).place(relx=.5, rely=.7,
+               bg="#ff4d4d", command=lambda: controller.show_frame("QuestionPage")).place(relx=.5, rely=.7,
                                                                                           anchor=CENTER)
