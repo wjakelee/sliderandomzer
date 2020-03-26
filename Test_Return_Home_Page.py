dc85e76e75852e27bfb25a0ec08f582a49a9fe9f
Jake Lee <jwlee@email.arizona.edu>
2020-03-26 12:55:17 -0700

    Changed button colors.

--- Test_Return_Home_Page.py
+++ Test_Return_Home_Page.py
@@ -37,7 +37,7 @@ class TestCompletePage(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
 
-        Label(self, text="Test Complete.", font='Arial 20 bold', bg="white").place(relx=0.5, rely=0.1, anchor='n')
+        Label(self, text="Test Complete.", font='Arial 20 bold').place(relx=0.5, rely=0.1, anchor='n')
 
         Button(self, text="Back To Home", fg="black", font="Arial 14 bold", width="25", height="3",
                bg="#01DF3A", command=lambda: controller.show_frame("StartPage")).place(relx=.5, rely=.4, anchor=CENTER)
