695ea2c0c6d760357247a0755b85bac66b3dede5
Jake Lee <jwlee@email.arizona.edu>
2020-04-13 19:53:50 -0700

    commented

--- Login_Page.py
+++ Login_Page.py
@@ -1,53 +1,18 @@
 from tkinter import *
 from tkinter import messagebox
-from Test_Setup import TestSetup
-from Set_Dat_Rand import SetDatRand
-from Start_Page import StartPage
-from Rand_And_Data import RandAndFile
-from Test_Intro_Page import TestIntro
-from Questionnaire_Page import QuestionPage
 
 
-# class is used here to run this page alone
-class Application(Tk):
-
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        self.title("Application")                               # window title
-        self.geometry('800x480')                                # window geometry
-        self.configure(background='white')                      # window background
-        container = Frame(self, background='white')                 # creates a frame in tkinter
-        container.place(anchor='nw', relwidth=1, relheight=1)       # location of frame covers entire window
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}                      # initializes a dictionary that will store different page frames
-        self.pages = {'StartPage': StartPage, 'Login': Login, 'SetDatRand': SetDatRand, 'TestSetup': TestSetup,
-                      'RandAndFile': RandAndFile, 'TestIntro': TestIntro, 'QuestionPage': QuestionPage}
-
-        # loop creates a frame for each page
-        for name, F in self.pages.items():                      # loops through dictionary 'pages'
-            frame = F(container, self)                              # assigns iterated frame to a variable 'frame'
-            frame.grid(row=0, column=0, sticky="nsew")              # frame positioning
-            self.frames[name] = frame                               # adds each page frame to dictionary of frames
-
-        self.show_frame("Login")              # calls show_frame method to raise StartPage frame when run
-
-    def show_frame(self, page):
-        frame = self.frames[page]               # selects frame to be raised, determined by button click
-        frame.tkraise()                         # raises frame called
-
-
-class Login(Frame):  # Sample Login page
+class Login(Frame):  # Login page
 
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
 
+        # prompt to enter pin
         Label(self, text="Please enter a PIN:", font='Arial 22 bold').place(relx=0.5, rely=0.15, anchor='n')
 
-        number = StringVar()
+        number = StringVar()    # initialize number variable type
 
+        # entry field for pin entry
         Entry(self, textvariable=number, background='#BDBDBD', justify=CENTER,
               font='Arial 18').place(relx=0.5, rely=0.375, anchor='n', height='50', width='200',)  # username entry
 
@@ -67,8 +32,3 @@ class Login(Frame):  # Sample Login page
                 controller.show_frame("SetDatRand")                   # raises SetDatRand frame
             else:
                 messagebox.showerror(title="Invalid", message="Incorrect PIN.")
-
-
-if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
