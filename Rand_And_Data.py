1e57f9dfd7efdaa4d800fea5f452a47b4a3315b1
Jake Lee <jwlee@email.arizona.edu>
2020-04-13 20:13:49 -0700

    comments

--- Rand_And_Data.py
+++ Rand_And_Data.py
@@ -4,45 +4,10 @@ from tkinter import filedialog
 from tkinter import messagebox
 import csv
 import random
-import operator
 from collections import MutableMapping
-from Test_Setup import TestSetup
-from Start_Page import StartPage
-from Test_Intro_Page import TestIntro
-from Questionnaire_Page import QuestionPage
 
 
-# class is used here to run this page alone
-class Application(Tk):
-
-    def __init__(self, *args, **kwargs):
-        Tk.__init__(self, *args, **kwargs)
-        self.title("Application")                               # window title
-        self.geometry('800x480')                                # window geometry
-        self.configure(background='white')                      # window background
-        container = Frame(self, background='white')                 # creates a frame in tkinter
-        container.place(anchor='nw', relwidth=1, relheight=1)       # location of frame covers entire window
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}                      # initializes a dictionary that will store different page frames
-        self.pages = {'StartPage': StartPage, 'TestSetup': TestSetup,
-                      'RandAndFile': RandAndFile, 'TestIntro': TestIntro, 'QuestionPage': QuestionPage}
-
-        # loop creates a frame for each page
-        for name, F in self.pages.items():                      # loops through dictionary 'pages'
-            frame = F(container, self)                              # assigns iterated frame to a variable 'frame'
-            frame.grid(row=0, column=0, sticky="nsew")              # frame positioning
-            self.frames[name] = frame                               # adds each page frame to dictionary of frames
-
-        self.show_frame("RandAndFile")              # calls show_frame method to raise StartPage frame when run
-
-    def show_frame(self, page):
-        frame = self.frames[page]               # selects frame to be raised, determined by button click
-        frame.tkraise()                         # raises frame called
-
-
+# choose to randomize order or export data page
 class RandAndFile(Frame):
 
     def __init__(self, parent, controller):
@@ -52,22 +17,23 @@ class RandAndFile(Frame):
 
         # generates a list of random numbers from 1-60 w/o repeats
         def generate_random():
-            with open('temporary_file.csv', 'r', newline="") as temp_file:
+            with open('temporary_file.csv', 'r', newline="") as temp_file:      # open file for reading
                 reader = csv.reader(temp_file)
                 import_list = []
                 for row in reader:
-                    import_list.append(row)
+                    import_list.append(row)         # creates list of case order
 
-            random.shuffle(import_list)
+            random.shuffle(import_list)         # shuffles randomization order
 
-            with open('temp_file_change.csv', 'w', newline="") as change_file:
+            with open('temp_file_change.csv', 'w', newline="") as change_file:      # open file for writing
                 writer = csv.writer(change_file)
                 for row in import_list:
-                    writer.writerow(row)
+                    writer.writerow(row)            # writes each row into file
 
             os.remove('temporary_file.csv')
-            os.rename('temp_file_change.csv', 'temporary_file.csv')
+            os.rename('temp_file_change.csv', 'temporary_file.csv')     # renames file
 
+            # checks if randomization list was generated or not, prompts user
             if import_list == []:
                 messagebox.showerror(title="No Order Selected!", message="Please set up the test prior to "
                                                                          "selecting a randomization order.")
@@ -75,7 +41,6 @@ class RandAndFile(Frame):
                 messagebox.showinfo(title="Order Selected", message="Randomization order has been selected and "
                                                                     "re-randomized from the test setup file "
                                                                     "(case_information.csv).")
-            # print(import_list)
 
         # imports order of numbers from temporary csv file
         def import_random():
@@ -99,13 +64,13 @@ class RandAndFile(Frame):
                     for line in reader:
                         writer.writerow(line)
 
+            # checks if randomization list was generated or not, prompts user
             if import_list == []:
                 messagebox.showerror(title="No Order Selected!", message="Please set up the test prior to "
                                                                          "selecting a randomization order.")
             else:
                 messagebox.showinfo(title="Order Selected", message="Randomization order has been selected from"
                                                                     " the test setup file (case_information.csv).")
-            # print(import_list)
 
         # export file with answers and barcodes
         def export():
@@ -115,6 +80,7 @@ class RandAndFile(Frame):
             with open('previous_ans.csv', 'r', newline='') as prev_ans_file:  # opens selected file for reading
                 reader = csv.reader(prev_ans_file)
                 next(reader)
+                # saves previous test answers to dictionary
                 for row in reader:
                     answers[row[0]] = {'Q1': row[1], 'Q2': row[2], 'Q3': row[3], 'Q4': row[4], 'Q5': row[5],
                                        'Q6': row[6], 'Comments':  row[7]}
@@ -123,27 +89,27 @@ class RandAndFile(Frame):
             # pulls barcode info from LAST test taken
             with open('temporary_file.csv', 'r', newline='') as temp_file:  # opens selected file for reading
                 temp_file_reader = csv.reader(temp_file)
+                # saves barcode info from last test to dictionary
                 for row in temp_file_reader:
                     barcode_info[row[0]] = {'he': row[1], 'nrc': row[2], 'ab': row[3]}
 
+            # merges barcode_info dictionary with answers dictionary
             def rec_merge(d1, d2):
-                '''
-                Update two dicts of dicts recursively,
-                if either mapping has leaves that are non-dicts,
-                the second's leaf overwrites the first's.
-                '''
+                # Update two dicts of dicts recursively, if either mapping has leaves that are non-dicts,
+                # the second's leaf overwrites the first's.
                 for k, v in d1.items():  # in Python 2, use .iteritems()!
                     if k in d2:
                         # this next check is the only difference!
                         if all(isinstance(e, MutableMapping) for e in (v, d2[k])):
                             d2[k] = rec_merge(v, d2[k])
-                        # we could further check types and merge as appropriate here.
                 d3 = d1.copy()
                 d3.update(d2)
                 return d3
 
+            # final dictionary with all necessary information from previous test
             final_dictionary = dict(rec_merge(answers, barcode_info))
 
+            # updates one dictionary
             def merge_dict(a, b):
                 a.update(b)
                 return a
@@ -152,6 +118,7 @@ class RandAndFile(Frame):
             selected_file = filedialog.asksaveasfilename(title="Choose where to dave your file.",
                                                          filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
 
+            # fields for final export file
             fields = ['Case', 'Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Comments', 'he', 'nrc', 'ab']
             with open(selected_file, 'w', newline='') as export_file:  # opens selected file for reading
                 writer = csv.DictWriter(export_file, fields)
@@ -161,7 +128,6 @@ class RandAndFile(Frame):
 
             # function adds name date and time to top of export file
             def prepend_line(file_name, line):
-                """ Insert given string as a new line at the beginning of a file """
                 # define name of temporary dummy file
                 dummy_file = file_name + '.bak'
                 # open original file in read mode and dummy file in write mode
@@ -176,17 +142,22 @@ class RandAndFile(Frame):
                 # Rename dummy file as the original file
                 os.rename(dummy_file, file_name)
 
+            # pulls user name/date information from last test
             with open('name_date.txt', 'r', newline='') as name_file:  # opens selected file for reading
                 name_date_time = name_file.read()
 
+            # adds name and date to top of file
             prepend_line(selected_file, name_date_time)
 
+        # button calls import_random function
         Button(self, text="Import Randomization Order", fg="black", font='Arial 14 bold', width='30',
                height='3', bg="#cc99ff", command=import_random).place(relx=0.5, rely=0.2, anchor=CENTER)
 
+        # button calls generate_random function
         Button(self, text="Generate Randomization Order", font='Arial 14 bold', width='30', height='3',
                fg="black", bg="#ffcc66", command=generate_random).place(relx=0.5, rely=0.45, anchor=CENTER)
 
+        # button calls export function
         Button(self, text="Export Data", font='Arial 14 bold', width='30', height='3',
                fg="black", bg="#99ff66", command=export).place(relx=0.5, rely=0.7, anchor=CENTER)
 
@@ -197,8 +168,3 @@ class RandAndFile(Frame):
         # button calls show_frame method and takes you back one page to SetDatRand
         Button(self, text="Back", fg="black", bg="#81DAF5", font="Arial 14", width='12', height='1',
                command=lambda: controller.show_frame("SetDatRand")).place(relx=0.8, rely=1.0, anchor=SE)
-
-
-if __name__ == "__main__":
-    app = Application()
-    app.mainloop()
