49b41140e572ba3153ad22dc81938b08806d983f
Jake Lee <jwlee@email.arizona.edu>
2020-04-13 20:17:27 -0700

    comments

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-class Bus(object):
+class Bus(object):      # used if hardware is not connected
     def __init__(self, bus_number):
         self.bus_number = bus_number
-        bus_num = hex(self.bus_number)
-        print('Bus created:', bus_num)
+        bus_num = hex(self.bus_number)  # sets bus number to hexi-decimal
+        print('Bus created:', bus_num)      # print statement to command window
 
+    # function prints whether pin is on or off
     def write_pin(self, pin_number, state):
         print('SIMULATION')
-        bus_num = hex(self.bus_number)
-        print('Bus:', bus_num)  # COMMENT WHEN USING HARDWARE
-        print('Pin:', pin_number)  # COMMENT WHEN USING HARDWARE
+        bus_num = hex(self.bus_number)      # sets bus number to hexi-decimal
+        print('Bus:', bus_num)
+        print('Pin:', pin_number)
 
         if state:
             print('ON')
@@ -18,10 +19,12 @@ class Bus(object):
 
         print('------')
 
+    # prints port direction
     def set_port_direction(self, a, b):
-        print('Write port direction')  # (0, 0x00)
+        print('Write port direction')     # (0, 0x00)
 
+    # prints that all ports are turned off
     def write_port(self, a, b):
         bus_num = hex(self.bus_number)
-        print('Write port', bus_num)    # (0, 0x00)
+        print('Write port', bus_num)       # (0, 0x00)
 
